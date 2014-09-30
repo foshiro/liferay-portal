718e93015a32f291b15cb534f07078737b6f2c93
Zsolt Szabo <zsolt.szabo@liferay.com>
2014-09-30 12:05:12 +0200

    LPS-50357 Validating startTime and endTime does not consider their time-zone

--- portal-web/docroot/html/portlet/layouts_admin/js/main.js
+++ portal-web/docroot/html/portlet/layouts_admin/js/main.js
@@ -1131,25 +1131,28 @@ AUI.add(
 
 							var startDate = startDatePicker.getDate();
 							var startTime = startTimePicker.getTime();
+							var startTimeTimeZoneOffset = startTimePicker.getTimeZoneOffset();
 
 							startDate.setHours(startTime.getHours());
 							startDate.setMinutes(startTime.getMinutes());
 							startDate.setSeconds(0);
 							startDate.setMilliseconds(0);
+							startDate = new Date(startDate.getTime() + startTimeTimeZoneOffset);
 
 							var endDate = endDatePicker.getDate();
 							var endTime = endTimePicker.getTime();
+							var endTimeTimeZoneOffset = endTimePicker.getTimeZoneOffset();
 
 							endDate.setHours(endTime.getHours());
 							endDate.setMinutes(endTime.getMinutes());
 							endDate.setSeconds(0);
 							endDate.setMilliseconds(0);
+							endDate = new Date(endDate.getTime() + endTimeTimeZoneOffset);
 
 							endsLater = ADate.isGreater(endDate, startDate);
 
 							var localeString = instance.get('locale');
 							var timeZoneString = instance.get('timeZone');
-
 							var today = new Date(new Date().toLocaleString(localeString, {timeZone: timeZoneString}));
 
 							endsInPast = ADate.isGreaterOrEqual(today, endDate);
