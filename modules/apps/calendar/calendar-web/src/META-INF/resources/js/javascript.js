f304c9d55a4ee3b6e7104297bce342c4517a174a
Inacio Nery <inacio.nery@liferay.com>
2015-10-08 14:02:02 -0300

    LPS-59218 Add hasWorkflowInstanceInProgress from CalendarBookingJSONObject into schedulerEvent

--- modules/apps/calendar/calendar-web/src/META-INF/resources/js/javascript.js
+++ modules/apps/calendar/calendar-web/src/META-INF/resources/js/javascript.js
@@ -265,6 +265,7 @@ AUI.add(
 						firstReminder: calendarBooking.firstReminder,
 						firstReminderType: calendarBooking.firstReminderType,
 						hasChildCalendarBookings: calendarBooking.hasChildCalendarBookings,
+						hasWorkflowInstanceInProgress: calendarBooking.hasWorkflowInstanceInProgress,
 						instanceIndex: calendarBooking.instanceIndex,
 						location: calendarBooking.location,
 						parentCalendarBookingId: calendarBooking.parentCalendarBookingId,
@@ -955,6 +956,11 @@ AUI.add(
 					},
 
 					hasChildCalendarBookings: {
+						validator: isBoolean,
+						value: false
+					},
+
+					hasWorkflowInstanceInProgress: {
 						validator: isBoolean,
 						value: false
 					},
@@ -1849,6 +1855,7 @@ AUI.add(
 								declineLinkEnabled: instance._hasWorkflowStatusPermission(schedulerEvent, CalendarWorkflow.STATUS_DENIED),
 								editing: editing,
 								endTime: templateData.endDate,
+								hasWorkflowInstanceInProgress:  schedulerEvent.get('hasWorkflowInstanceInProgress'),
 								instanceIndex: schedulerEvent.get('instanceIndex'),
 								maybeLinkEnabled: instance._hasWorkflowStatusPermission(schedulerEvent, CalendarWorkflow.STATUS_MAYBE),
 								permissions: permissions,
