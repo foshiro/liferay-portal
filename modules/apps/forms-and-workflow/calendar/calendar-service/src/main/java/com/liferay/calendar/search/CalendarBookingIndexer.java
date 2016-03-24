6d0f9450a9440fec0edb5fcaf7f8f2a60505866e
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2016-03-24 15:01:26 -0300

    LPS-64288 Resolve split package.

--- modules/apps/forms-and-workflow/calendar/calendar-service/src/main/java/com/liferay/calendar/search/CalendarBookingIndexer.java
+++ modules/apps/forms-and-workflow/calendar/calendar-service/src/main/java/com/liferay/calendar/search/CalendarBookingIndexer.java
@@ -41,7 +41,7 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.StringPool;
-import com.liferay.portal.search.FieldNames;
+import com.liferay.portal.search.constants.FieldNames;
 import com.liferay.trash.kernel.util.TrashUtil;
 
 import java.util.Locale;
