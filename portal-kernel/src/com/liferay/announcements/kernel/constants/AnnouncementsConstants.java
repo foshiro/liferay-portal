f6c480c77532b2185f7ac3cf4256ac3dfe9f4e51
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2016-03-24 09:13:19 -0300

    LPS-12345 Add constant

--- portal-kernel/src/com/liferay/announcements/kernel/constants/AnnouncementsConstants.java
+++ portal-kernel/src/com/liferay/announcements/kernel/constants/AnnouncementsConstants.java
@@ -22,4 +22,6 @@ public class AnnouncementsConstants {
 	public static final String CLASS_NAME_ALERTS_ENTRY =
 		"com.liferay.announcements.kernel.model.AlertsEntry";
 
+	public static final String OTHER_CONSTANT = "otherconstant";
+
 }
