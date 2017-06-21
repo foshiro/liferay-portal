33275d9b1d22eb3009267636dd4b4149cc24aa75
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2017-06-21 16:39:35 -0300

    LPS-73009 SF.

--- modules/apps/forms-and-workflow/calendar/calendar-service/src/main/java/com/liferay/calendar/internal/upgrade/v1_0_4/UpgradeClassNames.java
+++ modules/apps/forms-and-workflow/calendar/calendar-service/src/main/java/com/liferay/calendar/internal/upgrade/v1_0_4/UpgradeClassNames.java
@@ -70,11 +70,11 @@ public class UpgradeClassNames extends UpgradeKernelPackage {
 		try (LoggingTimer loggingTimer = new LoggingTimer()) {
 			StringBundler sb = new StringBundler(6);
 
-			sb.append("SELECT orp.resourcePermissionId FROM ");
-			sb.append("ResourcePermission orp, ResourcePermission nrp WHERE ");
-			sb.append("orp.companyId = nrp.companyId AND orp.scope = ");
-			sb.append("nrp.scope AND orp.primKey = nrp.primKey AND ");
-			sb.append("orp.roleId = nrp.roleId AND orp.name = ? AND nrp.name ");
+			sb.append("select orp.resourcePermissionId from ");
+			sb.append("ResourcePermission orp, ResourcePermission nrp where ");
+			sb.append("orp.companyId = nrp.companyId and orp.scope = ");
+			sb.append("nrp.scope and orp.primKey = nrp.primKey and ");
+			sb.append("orp.roleId = nrp.roleId and orp.name = ? and nrp.name ");
 			sb.append("= ?");
 
 			try (PreparedStatement ps = connection.prepareStatement(
