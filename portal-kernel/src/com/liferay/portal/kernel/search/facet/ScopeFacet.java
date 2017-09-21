f5d5819e7d764e87ab44c5c4c00079b7881eecdc
Bryan Engler <bryan.engler@liferay.com>
2017-09-21 12:29:22 -0700

    LPS-74655 portal-kernel: Deprecate ScopeFacet
    https://issues.liferay.com/browse/LPS-74655

--- portal-kernel/src/com/liferay/portal/kernel/search/facet/ScopeFacet.java
+++ portal-kernel/src/com/liferay/portal/kernel/search/facet/ScopeFacet.java
@@ -39,7 +39,9 @@ import java.util.List;
 
 /**
  * @author Raymond Augé
+ * @deprecated As of 7.0.0, with no direct replacement
  */
+@Deprecated
 public class ScopeFacet extends MultiValueFacet {
 
 	public ScopeFacet(SearchContext searchContext) {
