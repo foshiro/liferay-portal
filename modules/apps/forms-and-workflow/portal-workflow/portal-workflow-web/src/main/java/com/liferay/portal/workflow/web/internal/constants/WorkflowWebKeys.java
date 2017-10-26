brandizzi/com-liferay-portal-workflow

LPS-74975 / LPS-73731 SF: rename worflow dynamic includes to portlet tabs.
These classes do much more than being a mere dynamic include, and all
information they have are related to the displayed tabs, so I believe this name
would be better.

portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/constants/WorkflowWebKeys.java
@@ -31,13 +31,12 @@ public class WorkflowWebKeys {
 
 	public static final String WORKFLOW_TAB_DEFINITION_LINK = "schemes";
 
-	public static final String WORKFLOW_TAB_DYNAMIC_INCLUDES =
-		"WORKFLOW_TAB_DYNAMIC_INCLUDES";
-
 	public static final String WORKFLOW_TAB_INSTANCE = "monitoring";
 
 	public static final String WORKFLOW_TAB_MY_SUBMISSIONS = "my-submissions";
 
 	public static final String WORKFLOW_TAB_NAMES = "WORKFLOW_TAB_NAMES";
+
+	public static final String WORKFLOW_TABS = "WORKFLOW_TAB_DYNAMIC_INCLUDES";
 
 }

foshiro/liferay-portal

modules/apps/forms-and-workflow/portal-workflow/portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/internal/constants/WorkflowWebKeys.java
@@ -31,13 +31,12 @@ public class WorkflowWebKeys {
 
 	public static final String WORKFLOW_TAB_DEFINITION_LINK = "schemes";
 
-	public static final String WORKFLOW_TAB_DYNAMIC_INCLUDES =
-		"WORKFLOW_TAB_DYNAMIC_INCLUDES";
-
 	public static final String WORKFLOW_TAB_INSTANCE = "monitoring";
 
 	public static final String WORKFLOW_TAB_MY_SUBMISSIONS = "my-submissions";
 
 	public static final String WORKFLOW_TAB_NAMES = "WORKFLOW_TAB_NAMES";
+
+	public static final String WORKFLOW_TABS = "WORKFLOW_TABS";
 
 }