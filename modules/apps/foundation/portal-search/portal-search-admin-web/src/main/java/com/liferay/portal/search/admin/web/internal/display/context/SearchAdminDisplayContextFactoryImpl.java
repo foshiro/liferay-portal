23966194b5af7ce3925d449d317d0b3109f85237
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2017-11-23 17:29:48 -0300

    LPS-75873 portal-search-admin-web: Display search engine information.
    https://issues.liferay.com/browse/LPS-75873

---
+++ modules/apps/foundation/portal-search/portal-search-admin-web/src/main/java/com/liferay/portal/search/admin/web/internal/display/context/SearchAdminDisplayContextFactoryImpl.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.admin.web.internal.display.context;
+
+import com.liferay.portal.kernel.search.SearchEngine;
+import com.liferay.portal.kernel.search.SearchEngineHelper;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.search.web.information.SearchEngineInformation;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.portlet.PortletException;
+import javax.portlet.PortletPreferences;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+
+/**
+ * @author Adam Brandizzi
+ */
+@Component(immediate = true, service = SearchAdminDisplayContextFactory.class)
+public class SearchAdminDisplayContextFactoryImpl
+	implements SearchAdminDisplayContextFactory {
+
+	@Override
+	public SearchAdminDisplayContext create(
+			RenderRequest renderRequest, RenderResponse renderResponse,
+			PortletPreferences portletPreferences)
+		throws PortletException {
+
+		String searchEngineId = _searchEngineHelper.getDefaultSearchEngineId();
+
+		SearchEngine searchEngine = _searchEngineHelper.getSearchEngine(
+			searchEngineId);
+
+		String vendor = searchEngine.getVendor();
+
+		if (Validator.isNull(vendor)) {
+			vendor = "null";
+		}
+
+		SearchEngineInformation searchEngineInformation =
+			_searchEngineInformationMap.get(vendor);
+
+		return new SearchAdminDisplayContext(
+			searchEngineInformation.getStatusString());
+	}
+
+	@Reference(
+		cardinality = ReferenceCardinality.MULTIPLE,
+		policy = ReferencePolicy.DYNAMIC,
+		unbind = "unregisterSearchEngineInformation"
+	)
+	protected void registerSearchEngineInformation(
+		SearchEngineInformation searchEngineInformation,
+		Map<String, Object> properties) {
+
+		String vendor = MapUtil.getString(properties, "search.engine.impl");
+
+		_searchEngineInformationMap.put(vendor, searchEngineInformation);
+	}
+
+	protected void unregisterSearchEngineInformation(
+		SearchEngineInformation searchEngineInformation,
+		Map<String, Object> properties) {
+
+		String vendor = GetterUtil.getString(properties, "search.engine.impl");
+
+		_searchEngineInformationMap.remove(vendor);
+	}
+
+	@Reference
+	private SearchEngineHelper _searchEngineHelper;
+
+	private final Map<String, SearchEngineInformation>
+		_searchEngineInformationMap = new ConcurrentHashMap<>();
+
+}
---
+++ modules/apps/foundation/portal-search/portal-search-admin-web/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,40 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ page import="com.liferay.portal.kernel.util.WebKeys" %><%@
+page import="com.liferay.portal.search.admin.web.internal.display.context.SearchAdminDisplayContext" %>
+
+<%@ taglib uri="http://liferay.com/tld/aui" prefix="aui" %><%@
+taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui" %>
+
+<%
+SearchAdminDisplayContext searchAdminDisplayContext = (SearchAdminDisplayContext)request.getAttribute(WebKeys.PORTLET_DISPLAY_CONTEXT);
+%>
+
+<aui:nav-bar cssClass="collapse-basic-search" markupView="lexicon">
+	<aui:nav cssClass="navbar-nav">
+		<aui:nav-item
+			label="general"
+			selected="<%= true %>"
+		/>
+	</aui:nav>
+</aui:nav-bar>
+
+<div class="container-fluid-1280">
+	<strong><liferay-ui:message key="search-engine" /></strong><br>
+
+	<%= searchAdminDisplayContext.getStatusString() %>
+</div>
