e419a20397d5bb538b8213f07af32ccc5a6c30a4
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2017-11-23 17:35:27 -0300

    LPS-75873 portal-search-solr: Implement SearchEngineInformation for Solr.
    https://issues.liferay.com/browse/LPS-75873

---
+++ modules/apps/portal-search-solr/portal-search-solr/src/main/java/com/liferay/portal/search/solr/internal/information/SolrSearchEngineInformation.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.solr.internal.information;
+
+import com.liferay.portal.kernel.search.SearchEngine;
+import com.liferay.portal.search.information.SearchEngineInformation;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Adam Brandizzi
+ */
+@Component(
+	immediate = true, property = "search.engine.impl=Solr",
+	service = SearchEngineInformation.class
+)
+public class SolrSearchEngineInformation implements SearchEngineInformation {
+
+	@Override
+	public String getStatusString() {
+		return _searchEngine.getVendor();
+	}
+
+	@Reference(target = "(search.engine.impl=Solr)")
+	private SearchEngine _searchEngine;
+
+}
