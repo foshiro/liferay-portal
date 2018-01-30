ee5a275b0e1bc77d3e98ba807b0902d8b9c4513e
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2018-01-30 19:00:49 -0200

    LPS-74576 portal-search-web: model listener to create layout from prototype.
    https://issues.liferay.com/browse/LPS-74576

---
+++ modules/apps/foundation/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/configuration/SearchPageConfiguration.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.web.internal.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Adam Brandizzi
+ */
+@ExtendedObjectClassDefinition(category = "foundation")
+@Meta.OCD(
+	id = "com.liferay.portal.search.web.internal.configuration.SearchPageConfiguration",
+	localization = "content/Language", name = "search-page-configuration-name"
+)
+public interface SearchPageConfiguration {
+
+	@Meta.AD(
+		deflt = "search", description = "search-page-name-help",
+		name = "search-page-name", required = false
+	)
+	public String searchPageName();
+
+}
