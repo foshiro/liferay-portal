75446d62915517c19a6c4bb4f9e65699ea20a756
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2018-02-09 19:46:52 -0200

    LPS-74277 portal-search-web: clear custom range from URL: test to ensure FacetUtil.removeURLParameters() will stay correct.

---
+++ modules/apps/foundation/portal-search/portal-search-web/karma.conf.js
@@ -0,0 +1,18 @@
+var liferayKarmaAlloyConfig = require('liferay-karma-alloy-config');
+var liferayKarmaConfig = require('liferay-karma-config');
+
+module.exports = function(config) {
+	liferayKarmaConfig(config);
+
+	config.files = [];
+
+	liferayKarmaAlloyConfig(config);
+
+	config.files.push(
+		{
+			included: true,
+			pattern: 'src/main/resources/META-INF/resources/**/!(config).js'
+		},
+		'src/test/testJS/*_test.js'
+	);
+};
