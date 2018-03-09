19672ec4f93d7ce6299217cff712ab381d06500e
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2018-03-09 18:08:42 -0300

    LPS-78428 portal-search-web: Query suggestion factory.
    https://issues.liferay.com/browse/LPS-78428

---
+++ modules/apps/foundation/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/suggestions/QuerySuggestionFactory.java
@@ -0,0 +1,148 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.web.internal.suggestions;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.HttpUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.search.web.internal.search.suggest.KeywordsSuggestionHolder;
+import com.liferay.portal.search.web.internal.suggestions.display.QuerySuggestion;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author Adam Brandizzi
+ */
+public class QuerySuggestionFactory {
+
+	public QuerySuggestionFactory(
+		String searchURL, String parameterName, String keywords,
+		String spellCheckSuggestion, List<String> relatedSuggestions) {
+
+		_searchURL = searchURL;
+		_parameterName = parameterName;
+		_keywords = keywords;
+		_spellCheckSuggestion = spellCheckSuggestion;
+		_relatedSuggestions = relatedSuggestions;
+	}
+
+	public List<QuerySuggestion> getRelatedSuggestions() {
+		Stream<String> stream = _relatedSuggestions.stream();
+
+		return stream.map(
+			this::_buildQuerySuggestion
+		).filter(
+			Objects::nonNull
+		).collect(
+			Collectors.toList()
+		);
+	}
+
+	public QuerySuggestion getSpellCheckSuggestion() {
+		return _buildQuerySuggestion(_spellCheckSuggestion);
+	}
+
+	private String _buildFormattedQuery(
+		KeywordsSuggestionHolder keywordsSuggestionHolder) {
+
+		List<String> suggestedKeywords =
+			keywordsSuggestionHolder.getSuggestedKeywords();
+
+		List<String> elements = new ArrayList<>(suggestedKeywords.size());
+
+		for (String keyword : suggestedKeywords) {
+			elements.add(_getKeywordElement(keywordsSuggestionHolder, keyword));
+		}
+
+		return String.join(" ", elements);
+	}
+
+	private QuerySuggestion _buildQuerySuggestion(String suggestion) {
+		if (!_isValidSuggestion(suggestion)) {
+			return null;
+		}
+
+		KeywordsSuggestionHolder keywordsSuggestionHolder =
+			new KeywordsSuggestionHolder(suggestion, _keywords);
+
+		QuerySuggestion querySuggestion = new QuerySuggestion();
+
+		querySuggestion.setFormattedQuery(
+			_buildFormattedQuery(keywordsSuggestionHolder));
+		querySuggestion.setSearchURL(_buildSearchURL(keywordsSuggestionHolder));
+
+		return querySuggestion;
+	}
+
+	private String _buildSearchURL(
+		KeywordsSuggestionHolder keywordsSuggestionHolder) {
+
+		List<String> suggestedKeywords =
+			keywordsSuggestionHolder.getSuggestedKeywords();
+
+		String parameterValue = String.join(
+			StringPool.SPACE, suggestedKeywords);
+
+		return HttpUtil.setParameter(
+			_searchURL, _parameterName, parameterValue);
+	}
+
+	private String _getKeywordElement(
+		KeywordsSuggestionHolder keywordsSuggestionHolder, String keyword) {
+
+		StringBundler sb = new StringBundler(5);
+
+		sb.append("<span class=\"");
+
+		String keywordCssClass = "unchanged-keyword";
+
+		if (keywordsSuggestionHolder.hasChanged(keyword)) {
+			keywordCssClass = "changed-keyword";
+		}
+
+		sb.append(keywordCssClass);
+
+		sb.append("\">");
+		sb.append(HtmlUtil.escape(keyword));
+		sb.append("</span>");
+
+		return sb.toString();
+	}
+
+	private boolean _isValidSuggestion(String suggestion) {
+		if (Objects.equals(_keywords, suggestion)) {
+			return false;
+		}
+
+		if (Validator.isNull(suggestion)) {
+			return false;
+		}
+
+		return true;
+	}
+
+	private final String _keywords;
+	private final String _parameterName;
+	private final List<String> _relatedSuggestions;
+	private final String _searchURL;
+	private final String _spellCheckSuggestion;
+
+}
