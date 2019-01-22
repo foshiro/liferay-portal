brandonyee333/test

LPS-89060 layout-impl: Integrate Layout into new indexer model; Integrate Layout into new permission model; Deprecate Layout legacy indexer.
https://issues.liferay.com/browse/LPS-89060

modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/search/LayoutModelSummaryContributor.java
@@ -1,0 +1,97 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.internal.search;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Summary;
+import com.liferay.portal.kernel.search.highlight.HighlightUtil;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.search.spi.model.result.contributor.ModelSummaryContributor;
+
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Vagner B.C
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.portal.kernel.model.Layout",
+	service = ModelSummaryContributor.class
+)
+public class LayoutModelSummaryContributor implements ModelSummaryContributor {
+
+	@Override
+	public Summary getSummary(
+		Document document, Locale locale, String snippet) {
+
+		String localizedFieldName = Field.getLocalizedName(locale, Field.NAME);
+
+		if (Validator.isNull(document.getField(localizedFieldName))) {
+			locale = LocaleUtil.fromLanguageId(
+				document.get(Field.DEFAULT_LANGUAGE_ID));
+		}
+
+		String name = document.get(
+			locale, Field.SNIPPET + StringPool.UNDERLINE + Field.TITLE,
+			Field.TITLE);
+
+		String content = document.get(locale, Field.CONTENT);
+
+		content = StringUtil.replace(
+			content, _HIGHLIGHT_TAGS, _ESCAPE_SAFE_HIGHLIGHTS);
+
+		content = HtmlUtil.extractText(content);
+
+		content = StringUtil.replace(
+			content, _ESCAPE_SAFE_HIGHLIGHTS, _HIGHLIGHT_TAGS);
+
+		snippet = document.get(
+			locale, Field.SNIPPET + StringPool.UNDERLINE + Field.CONTENT);
+
+		Set<String> highlights = new HashSet<>();
+
+		HighlightUtil.addSnippet(document, highlights, snippet, "temp");
+
+		content = HighlightUtil.highlight(
+			content, ArrayUtil.toStringArray(highlights),
+			HighlightUtil.HIGHLIGHT_TAG_OPEN,
+			HighlightUtil.HIGHLIGHT_TAG_CLOSE);
+
+		Summary summary = new Summary(locale, name, content);
+
+		summary.setMaxContentLength(200);
+
+		return summary;
+	}
+
+	private static final String[] _ESCAPE_SAFE_HIGHLIGHTS = {
+		"[@HIGHLIGHT1@]", "[@HIGHLIGHT2@]"
+	};
+
+	private static final String[] _HIGHLIGHT_TAGS = {
+		HighlightUtil.HIGHLIGHT_TAG_OPEN, HighlightUtil.HIGHLIGHT_TAG_CLOSE
+	};
+
+}

foshiro/liferay-portal

modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/search/spi/model/result/contributor/LayoutModelSummaryContributor.java
@@ -1,0 +1,101 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.internal.search.spi.model.result.contributor;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Summary;
+import com.liferay.portal.kernel.search.highlight.HighlightUtil;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.Html;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.search.spi.model.result.contributor.ModelSummaryContributor;
+
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Vagner B.C
+ */
+@Component(
+	immediate = true,
+	property = "indexer.class.name=com.liferay.portal.kernel.model.Layout",
+	service = ModelSummaryContributor.class
+)
+public class LayoutModelSummaryContributor implements ModelSummaryContributor {
+
+	@Override
+	public Summary getSummary(
+		Document document, Locale locale, String snippet) {
+
+		String localizedFieldName = Field.getLocalizedName(locale, Field.NAME);
+
+		if (Validator.isNull(document.getField(localizedFieldName))) {
+			locale = LocaleUtil.fromLanguageId(
+				document.get(Field.DEFAULT_LANGUAGE_ID));
+		}
+
+		String name = document.get(
+			locale, Field.SNIPPET + StringPool.UNDERLINE + Field.TITLE,
+			Field.TITLE);
+
+		String content = document.get(locale, Field.CONTENT);
+
+		content = StringUtil.replace(
+			content, _HIGHLIGHT_TAGS, _ESCAPE_SAFE_HIGHLIGHTS);
+
+		content = _html.extractText(content);
+
+		content = StringUtil.replace(
+			content, _ESCAPE_SAFE_HIGHLIGHTS, _HIGHLIGHT_TAGS);
+
+		snippet = document.get(
+			locale, Field.SNIPPET + StringPool.UNDERLINE + Field.CONTENT);
+
+		Set<String> highlights = new HashSet<>();
+
+		HighlightUtil.addSnippet(document, highlights, snippet, "temp");
+
+		content = HighlightUtil.highlight(
+			content, ArrayUtil.toStringArray(highlights),
+			HighlightUtil.HIGHLIGHT_TAG_OPEN,
+			HighlightUtil.HIGHLIGHT_TAG_CLOSE);
+
+		Summary summary = new Summary(locale, name, content);
+
+		summary.setMaxContentLength(200);
+
+		return summary;
+	}
+
+	private static final String[] _ESCAPE_SAFE_HIGHLIGHTS = {
+		"[@HIGHLIGHT1@]", "[@HIGHLIGHT2@]"
+	};
+
+	private static final String[] _HIGHLIGHT_TAGS = {
+		HighlightUtil.HIGHLIGHT_TAG_OPEN, HighlightUtil.HIGHLIGHT_TAG_CLOSE
+	};
+
+	@Reference
+	private Html _html;
+
+}