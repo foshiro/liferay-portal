33b4f253ce9896d99f0e0fdc04e3f75df3f34900
Filipe Oshiro <filipe.oshiro@liferay.com>
2019-02-28 13:57:36 -0300

    LPS-90876 portal-search-admin-web: add alloy editor and copy button

--- modules/apps/portal-search/portal-search-admin-web/src/main/resources/META-INF/resources/js/FieldMappings.es.js
+++ modules/apps/portal-search/portal-search-admin-web/src/main/resources/META-INF/resources/js/FieldMappings.es.js
@@ -1,15 +1,41 @@
-import PortletBase from 'frontend-js-web/liferay/PortletBase.es';
+import 'clay-button';
 import 'metal';
 import 'metal-component';
-import Soy from 'metal-soy';
 import {Config} from 'metal-state';
+import Soy from 'metal-soy';
+import PortletBase from 'frontend-js-web/liferay/PortletBase.es';
 
 import templates from './FieldMappings.soy';
 
 class FieldMappings extends PortletBase {
 
-	created() {
+	attached() {
+		AUI().use(
+			'aui-ace-editor',
+			A => {
+				const editor = new A.AceEditor({
+					boundingBox: this.refs.wrapper,
+					highlightActiveLine: false,
+					mode: 'json',
+					value: '{}',
+					tabSize: 4
+				}).render();
+			}
+		);
+	}
 
+	_selectText() {
+		let copyTextArea = document.querySelector(".ace_text-input")
+		copyTextArea.focus();
+		copyTextArea.select();
+
+		setTimeout(
+			function() {
+				document.execCommand("copy");
+				Liferay.Portal.ToolTip.show(document.querySelector(".btn-copy"), 'Copied');
+			},
+			0
+		);
 	}
 }
 
