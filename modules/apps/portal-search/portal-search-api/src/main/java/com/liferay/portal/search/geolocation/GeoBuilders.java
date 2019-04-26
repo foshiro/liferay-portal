brandonyee333/test

LPS-90614 portal-search-api: Geo: decouple interfaces from implementation
https://issues.liferay.com/browse/LPS-90614

Co-authored-by: weicao <wade.cao@liferay.com>
Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>

modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/geolocation/GeoBuilders.java
@@ -1,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.geolocation;
+
+import aQute.bnd.annotation.ProviderType;
+
+import java.util.List;
+
+/**
+ * @author Wade Cao
+ * @author André de Oliveira
+ */
+@ProviderType
+public interface GeoBuilders {
+
+	public CircleShape circleShape(
+		Coordinate center, GeoDistance radiusGeoDistance);
+
+	public CircleShapeBuilder circleShapeBuilder();
+
+	public Coordinate coordinate(double x, double y);
+
+	public Coordinate coordinate(double x, double y, double z);
+
+	public EnvelopeShape envelopeShape(
+		Coordinate topLeft, Coordinate bottomRight);
+
+	public EnvelopeShapeBuilder envelopeShapeBuilder();
+
+	public GeoDistance geoDistance(double distance);
+
+	public GeoDistance geoDistance(double distance, DistanceUnit distanceUnit);
+
+	public GeoLocationPoint geoLocationPoint(double latitude, double longitude);
+
+	public GeoLocationPoint geoLocationPoint(long geoHash);
+
+	public GeoLocationPoint geoLocationPoint(String geoHash);
+
+	public GeometryCollectionShapeBuilder geometryCollectionShapeBuilder();
+
+	public LineStringShape lineStringShape(List<Coordinate> coordinates);
+
+	public LineStringShapeBuilder lineStringShapeBuilder();
+
+	public MultiLineStringShapeBuilder multiLineStringShapeBuilder();
+
+	public MultiPointShape multiPointShape(List<Coordinate> coordinates);
+
+	public MultiPointShapeBuilder multiPointShapeBuilder();
+
+	public MultiPolygonShape multiPolygonShape(Orientation orientation);
+
+	public MultiPolygonShapeBuilder multiPolygonShapeBuilder();
+
+	public PointShape pointShape(Coordinate coordinate);
+
+	public PointShapeBuilder pointShapeBuilder();
+
+	public PolygonShape polygonShape(
+		LineStringShape shellLineStringShape, Orientation orientation);
+
+	public PolygonShapeBuilder polygonShapeBuilder();
+
+}

foshiro/liferay-portal

modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/geolocation/GeoBuilders.java
@@ -1,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.geolocation;
+
+import aQute.bnd.annotation.ProviderType;
+
+import java.util.List;
+
+/**
+ * @author Wade Cao
+ * @author André de Oliveira
+ */
+@ProviderType
+public interface GeoBuilders {
+
+	public CircleShape circleShape(
+		Coordinate centerCoordinate, GeoDistance radiusGeoDistance);
+
+	public CircleShapeBuilder circleShapeBuilder();
+
+	public Coordinate coordinate(double x, double y);
+
+	public Coordinate coordinate(double x, double y, double z);
+
+	public EnvelopeShape envelopeShape(
+		Coordinate topLeftCoordinate, Coordinate bottomRightCoordinate);
+
+	public EnvelopeShapeBuilder envelopeShapeBuilder();
+
+	public GeoDistance geoDistance(double distance);
+
+	public GeoDistance geoDistance(double distance, DistanceUnit distanceUnit);
+
+	public GeoLocationPoint geoLocationPoint(double latitude, double longitude);
+
+	public GeoLocationPoint geoLocationPoint(long geoHash);
+
+	public GeoLocationPoint geoLocationPoint(String geoHash);
+
+	public GeometryCollectionShapeBuilder geometryCollectionShapeBuilder();
+
+	public LineStringShape lineStringShape(List<Coordinate> coordinates);
+
+	public LineStringShapeBuilder lineStringShapeBuilder();
+
+	public MultiLineStringShapeBuilder multiLineStringShapeBuilder();
+
+	public MultiPointShape multiPointShape(List<Coordinate> coordinates);
+
+	public MultiPointShapeBuilder multiPointShapeBuilder();
+
+	public MultiPolygonShape multiPolygonShape(Orientation orientation);
+
+	public MultiPolygonShapeBuilder multiPolygonShapeBuilder();
+
+	public PointShape pointShape(Coordinate coordinate);
+
+	public PointShapeBuilder pointShapeBuilder();
+
+	public PolygonShape polygonShape(
+		LineStringShape shellLineStringShape, Orientation orientation);
+
+	public PolygonShapeBuilder polygonShapeBuilder();
+
+}