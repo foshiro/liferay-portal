b9ab84033cb1b04b61774e39b8bdac1a822f8d62
Filipe Oshiro <filipe.oshiro@liferay.com>
2019-05-20 09:59:24 -0300

    LPS-95694 portal-search-synonyms-web: Rename module and all class name

---
+++ modules/apps/portal-search/portal-search-synonyms-web/src/main/java/com/liferay/portal/search/synonyms/web/internal/constants/SynonymsPortletKeys.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.synonyms.web.internal.constants;
+
+/**
+ * @author Filipe Oshiro
+ */
+public class SynonymsPortletKeys {
+
+	public static final String SYNONYMS =
+		"com_liferay_portal_search_synonyms_web_portlet_SynonymsPortlet";
+
+	public static final String SYNONYM_SETS_DISPLAY_CONTEXT =
+		"SYNONYM_SETS_DISPLAY_CONTEXT";
+
+}
